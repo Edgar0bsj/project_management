2279259a56bb730650924c7bda5dc1ec11c614ab
Edgar barbosa da silva junior <edgarbsj.dev@gmail.com>
2024-09-13 16:25:32 -0300

    ajuste

--- app/Views/Admin/principal.php
+++ app/Views/Admin/principal.php
@@ -4,24 +4,64 @@
  * @var CodeIgniter\View\View $this
  */
 ?>
-<?=$this->extend('Templates/base_admin');?>
+<?= $this->extend('Templates/base_admin'); ?>
 <!------TITULO---------->
-<?= $this->section('title')?>
+<?= $this->section('title') ?>
 <?= $title ?>
-<?= $this->endSection()?>
+<?= $this->endSection() ?>
 <!------/TITULO---------->
 
 <!------STYLE------>
-<?= $this->section('style')?>
-<?= $this->endSection()?>
+<?= $this->section('style') ?>
+<?= $this->endSection() ?>
 <!------/STYLE------>
 
 <!------MAIN------->
-<?= $this->section('main')?>
-
-<H1>ESTA DENTRO</H1>
+<?= $this->section('main') ?>
 
 
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Principal</h1>
+    <div class="card mb-4">
+        <div class="card-body">
+            <P>DESCRIÇÃO</P>
+        </div>
+    </div>
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fas fa-table me-1"></i>
+            Clientes
+        </div>
+        <div class="card-body">
+            <table id="datatablesSimple">
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Telefone</th>
+                    </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Email</th>
+                        <th>Telefone</th>
+                    </tr>
+                </tfoot>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Tiger Nixon</td>
+                        <td>tiger@azmail.com</td>
+                        <td>(21) 912345678</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
 
 
@@ -29,5 +69,19 @@
 
 
 
-<?= $this->endSection()?>
+
+
+<?= $this->endSection() ?>
 <!------/MAIN------->
+
+<!------SCRIPT------->
+<?= $this->section('script') ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+<script src="<?= site_url('dashboard/js/scripts.js') ?>"></script>
+<script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js"
+    crossorigin="anonymous"></script>
+<script src="<?= site_url('dashboard/js') ?>/datatables-simple-demo.js"></script>
+<?= $this->endSection() ?>
+<!------/SCRIPT------->
